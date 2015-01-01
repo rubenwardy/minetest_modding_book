df92b4e95d351af22d5be9a6390f21df33c71c42
rubenwardy <rubenwardy@gmail.com>
2015-01-01 17:14:18 +0000

    Add printable version

--- static/script.js
+++ static/script.js
@@ -1,4 +1,8 @@
 $(function() {
+	$("#printable").click(function() {
+		$("body").addClass("printable");
+	});
+
 	return $("h2, h3, h4, h5, h6").each(function(i, el) {
 		var $el, icon, id;
 		$el = $(el);
