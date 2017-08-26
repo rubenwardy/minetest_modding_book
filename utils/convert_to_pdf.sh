381097c839bcd31a503d0ae6ace10fc72d6b13cd
rubenwardy <rubenwardy@gmail.com>
2017-08-26 18:01:49 +0100

    Use headless Chrome instead of wkhtmltopdf

--- utils/convert_to_pdf.sh
+++ utils/convert_to_pdf.sh
@@ -6,5 +6,20 @@ oldCWD="$PWD"
 cd $(dirname $0)
 cd ../en/
 
-wkhtmltopdf --print-media-type {% for link in site.data.links_en %}{% if link.link %}{{ link.link }} {% endif %}{% endfor %} out.pdf
-mv out.pdf $oldCWD/book.pdf
+#wkhtmltopdf --print-media-type {% for link in site.data.links_en %}{% if link.link %}{{ link.link }} {% endif %}{% endfor %} out.pdf
+#mv out.pdf $oldCWD/book.pdf
+
+{% for link in site.data.links_en %}
+{% if link.link %}
+/opt/google/chrome/google-chrome --headless  --disable-gpu --print-to-pdf file:///${PWD}/{{ link.link }}
+
+{% if link.num %}
+mv output.pdf page_{{ link.num }}.pdf
+{% else %}
+mv output.pdf page_0_{{ link.link | replace:".","_" }}.pdf
+{% endif %}
+
+{% endif %}
+{% endfor %}
+
+pdfunite page*.pdf ${oldCWD}/book.pdf
