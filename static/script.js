4822793af9b83ba9e0cac1248fd630fd7f2b12a6
rubenwardy <rubenwardy@gmail.com>
2017-08-27 19:47:29 +0100

    Fix blocking Javascript

--- static/script.js
+++ static/script.js
@@ -1,19 +1,30 @@
 // @license magnet:?xt=urn:btih:90dc5c0be029de84e523b9b3922520e79e0e6f08&dn=cc0.txt CC0
 // This script is licensed under CC0
 
-$(function() {
-	$("#printable").click(function() {
-		$("body").addClass("printable");
-	});
-
-	return $("h2, h3, h4, h5, h6").each(function(i, el) {
-		var $el, icon, id;
-		$el = $(el);
-		id = $el.attr('id');
-		if (id) {
-			return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html("#"));
+(function() {
+	function setJQueryTrigger() {
+		if (window.$ == undefined) {
+			setTimeout(setJQueryTrigger, 70);
+			return;
 		}
-	});
-});
+
+		$(function() {
+			$("#printable").click(function() {
+				$("body").addClass("printable");
+			});
+
+			return $("h2, h3, h4, h5, h6").each(function(i, el) {
+				var $el, icon, id;
+				$el = $(el);
+				id = $el.attr('id');
+				if (id) {
+					return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html("#"));
+				}
+			});
+		});
+	}
+
+	setJQueryTrigger();
+})();
 
 // @license-end
